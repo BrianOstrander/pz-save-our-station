e9538641f6786984398b66c7235212c44a2b87a7
Brian Ostrander <Ostrander.Brian@gmail.com>
2022-01-05 16:57:58 -0800

    Added spawnpoints.

--- release/Save Our Station/Contents/mods/Save Our Station - Knox Country/media/maps/SaveOurStation_KnoxCountry_World/spawnpoints.lua
+++ release/Save Our Station/Contents/mods/Save Our Station - Knox Country/media/maps/SaveOurStation_KnoxCountry_World/spawnpoints.lua
@@ -1,3 +1,40 @@
 function SpawnPoints()
-return {}
+return {
+  constructionworker = {
+    { worldX = 24, worldY = 18, posX = 189, posY = 71, posZ = 0 },
+    { worldX = 35, worldY = 24, posX = 244, posY = 115, posZ = 0 },
+    { worldX = 34, worldY = 34, posX = 43, posY = 45, posZ = 0 },
+    { worldX = 25, worldY = 38, posX = 189, posY = 61, posZ = 0 }
+  },
+  fireofficer = {
+    { worldX = 24, worldY = 18, posX = 189, posY = 71, posZ = 0 },
+    { worldX = 35, worldY = 24, posX = 244, posY = 115, posZ = 0 },
+    { worldX = 34, worldY = 34, posX = 43, posY = 45, posZ = 0 },
+    { worldX = 25, worldY = 38, posX = 189, posY = 61, posZ = 0 }
+  },
+  parkranger = {
+    { worldX = 24, worldY = 18, posX = 189, posY = 71, posZ = 0 },
+    { worldX = 35, worldY = 24, posX = 244, posY = 115, posZ = 0 },
+    { worldX = 34, worldY = 34, posX = 43, posY = 45, posZ = 0 },
+    { worldX = 25, worldY = 38, posX = 189, posY = 61, posZ = 0 }
+  },
+  policeofficer = {
+    { worldX = 24, worldY = 18, posX = 189, posY = 71, posZ = 0 },
+    { worldX = 35, worldY = 24, posX = 244, posY = 115, posZ = 0 },
+    { worldX = 34, worldY = 34, posX = 43, posY = 45, posZ = 0 },
+    { worldX = 25, worldY = 38, posX = 189, posY = 61, posZ = 0 }
+  },
+  securityguard = {
+    { worldX = 24, worldY = 18, posX = 189, posY = 71, posZ = 0 },
+    { worldX = 35, worldY = 24, posX = 244, posY = 115, posZ = 0 },
+    { worldX = 34, worldY = 34, posX = 43, posY = 45, posZ = 0 },
+    { worldX = 25, worldY = 38, posX = 189, posY = 61, posZ = 0 }
+  },
+  unemployed = {
+    { worldX = 24, worldY = 18, posX = 189, posY = 71, posZ = 0 },
+    { worldX = 35, worldY = 24, posX = 244, posY = 115, posZ = 0 },
+    { worldX = 34, worldY = 34, posX = 43, posY = 45, posZ = 0 },
+    { worldX = 25, worldY = 38, posX = 189, posY = 61, posZ = 0 }
+  }
+}
 end
