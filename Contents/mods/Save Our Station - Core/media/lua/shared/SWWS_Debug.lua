18c45a99a1f4a62fac6ddb21957a105d1a751483
Brian Ostrander <Ostrander.Brian@gmail.com>
2023-03-09 12:59:06 -0800

    Simplified debug file and added battery recharging.

--- Contents/mods/Save Our Station - Core/media/lua/shared/SWWS_Debug.lua
+++ Contents/mods/Save Our Station - Core/media/lua/shared/SWWS_Debug.lua
@@ -13,42 +13,60 @@ SWWS_Debug.isInitialized = false
 -- DO NOT MESS WITH UNLESS YOU KNOW WHAT YOU ARE DOING!
 -------------------------------------------------------
 
-function SWWS_DebugSetup()
+-- Forces mod to reinitialize game state every time a game is loaded (= Clears any currently-ongoing stages)
+SWWS_Debug.forceInitialize = false
 
-	-- Forces mod to reinitialize game state every time a game is loaded (= Clears any currently-ongoing stages)
-	SWWS_Debug.forceInitialize = false
+-- Ignores if the power is on or off, and forces breakdown checks every hour.
+SWWS_Debug.ignoreRequirePowerShutoff = false
 
-	-- Ignores if the power is on or off, and forces breakdown checks every hour.
-	SWWS_Debug.ignoreRequirePowerShutoff = false
+-- Force stage time to a single hour.
+SWWS_Debug.forceMinimumTime = false
 
-	-- Force stage time to a single hour.
-	SWWS_Debug.forceMinimumTime = false
+-- Forces the fatal pool to be chosen every time.
+SWWS_Debug.forceFatal = false
 
-	-- Forces the fatal pool to be chosen every time.
-	SWWS_Debug.forceFatal = false
+-- Forces the NON-fatal (Interruption) pool to be chosen every time.
+-- Important: This IGNORES the "Enable Interruptions?" mod option.
+SWWS_Debug.forceNonFatal = false
 
-	-- Forces the NON-fatal (Interruption) pool to be chosen every time.
-	-- Important: This IGNORES the "Enable Interruptions?" mod option.
-	SWWS_Debug.forceNonFatal = false
+-- Makes batteries on military walkies last forever when enabled.
+SWWS_Debug.infiniteBattery = true
 
-	-- Enables logging of various events
-	SWWS_Debug.logging = false
+-- Enables logging of various events
+SWWS_Debug.logging = false
 
-	-------------------------------------------------------
+-------------------------------------------------------
 
-	if SWWS_Debug.logging then
+if SWWS_Debug.logging then
 
-		local gameType = "Singleplayer"
+	local gameType = "Singleplayer"
 
-		if isServer() then
-			gameType = "Server"
-		elseif isClient() then
-			gameType = "Client / Host"
-		end 
+	if isServer() then
+		gameType = "Server"
+	elseif isClient() then
+		gameType = "Client / Host"
+	end 
 
-		print("SWWS: Game Type [" .. gameType .. "], running [" .. SWWS_Debug.version .. "]")
+	print("SWWS: Game Type [" .. gameType .. "], running [" .. SWWS_Debug.version .. "]")
 
-	end
 end
 
-pcall(SWWS_DebugSetup)
+function SWWS_Debug.RechargeRadios()
+	local players = {}
+	if not isClient() and not isServer() then
+		-- Is singleplayer
+		players = { getPlayer() }
+    else
+		players = getOnlinePlayers()
+	end
+
+	for _, player in ipairs(players) do
+		local walkie = getPlayer():getInventory():getItemFromType("Radio.WalkieTalkie5", true, true)
+		if walkie then
+			walkie:getDeviceData():setPower(1)	
+		end
+	end
+end
+if SWWS_Debug.infiniteBattery then
+	Events.EveryTenMinutes.Add(SWWS_Debug.RechargeRadios)
+end
